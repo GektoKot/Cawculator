cada78c2fa8acc76827c6615d670186fd1c5d5dc
Димас <52570598+GektoKot@users.noreply.github.com>
2021-01-15 15:39:45 +0100

    comments added

--- src/main/Cawculator.java
+++ src/main/Cawculator.java
@@ -1,93 +1,105 @@
 package main;
-
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.StringReader;
+//все что нам надо для счастья
 import java.util.Stack;
 
+//Самый главный класс после Мэйна
 public class Cawculator {
-
-    public static String expressionToRPN(String expression) {
-        String extendedExpression = extendingExpression(expression);
-//        StringBuilder rpnResult = new StringBuilder();
-        String rpnResult = "";
+    //Обощающий метод
+    public static double cawculateIt(String expression) {
+        return rpnToAnswer(expressionToRPN(expression));
+    }
+    //Перевод нормального выражения в ОПН
+    private static String expressionToRPN(String expression) {
+        //перерабатываем выражение в более дружелюбное к унарным минусам
+        String extendedExpression = extendingExpressionForUnaryMinus(expression);
+        //стрингбилдер куда будем аппендить результаты проверок
+        StringBuilder rpnResult = new StringBuilder();
+        //стак для операций
         Stack<Character> operationStack = new Stack<>();
+        //пробегаем циклом по всему выражению по каждому символу
         for (int i = 0; i < extendedExpression.length(); i++) {
+            //вынесем отдельно переменную приоритета символа
             int priority = getPriority(extendedExpression.charAt(i));
-
+            //если число, то просто добавляем символ к результату
             if (priority == 0) {
-//                rpnResult.append(extendedExpression.charAt(i));
-                rpnResult += extendedExpression.charAt(i);
+                rpnResult.append(extendedExpression.charAt(i));
                 continue;
             }
+            //если открытая скобка, то добавляем ее в стэк
             if (priority == 1) {
                 operationStack.push(extendedExpression.charAt(i));
                 continue;
             }
+            //если какя-либо операция, то...
             if (priority > 1) {
-//                rpnResult.append(" ");
-                rpnResult += " ";
-
+                //добавляем пробел для разделения операндов
+                rpnResult.append(" ");
+                //в цикле пока не пустой стэк...
                 while (!operationStack.empty()) {
+                    //если приоритет последней операции в стэке больше или равно, то добавляем ее в результат
                     if (getPriority(operationStack.peek()) >= priority) {
-//                        rpnResult.append(operationStack.pop());
-                        rpnResult += operationStack.pop();
+                        rpnResult.append(operationStack.pop());
+                        //в противном случае прерываем цикл
                     } else {
                         break;
                     }
                 }
+                //засовываем в стэк операцию
                 operationStack.push(extendedExpression.charAt(i));
                 continue;
             }
+            //если нарвались на закрывающуюся скобку, то ...
             if (priority == -1) {
-//                rpnResult.append(" ");
-                rpnResult += " ";
+                //добавляем пробел для разделения операндов
+                rpnResult.append(" ");
+                //в цикле чекаем приоритет последней операции в стэки и если он не равен 1,
+                //то добавляем к результату операцию из стэка
                 while (getPriority(operationStack.peek()) != 1) {
-//                    rpnResult.append(operationStack.pop());
-                    rpnResult += operationStack.pop();
+                    rpnResult.append(operationStack.pop());
                 }
+                //в конце просто выкидываем скобку
                 operationStack.pop();
             }
         }
+        //пока стэк с операциями не опустеет забиваем все в результат
         while (!operationStack.empty()) {
-//            rpnResult.append(operationStack.pop());
-            rpnResult += operationStack.pop();
+            rpnResult.append(operationStack.pop());
         }
-        return rpnResult;
-//        return rpnResult.toString();
+        //возвращаем готовенькую ОПН
+        return rpnResult.toString();
     }
-
-    public static double rpnToAnswer(String rpn) {
-//        StringBuilder operand = new StringBuilder();
-        String operand = "";
-
+    //Рассчет на основе ОПН, возвращает ответ в виде дабл
+    private static double rpnToAnswer(String rpn) {
+        //новый стрингбилдер под операнд
+        StringBuilder operand = new StringBuilder();
+        //стэк с результатом
         Stack<Double> resultStack = new Stack<>();
-
-
+        //цикл где бегаем по ОПН по каждому символу
         for (int i = 0; i < rpn.length(); i++) {
-
+            //если пробел, то смело переходим к новой итерации
             if (rpn.charAt(i) == ' ') {
                 continue;
             }
+            //если число, то...
             if (getPriority(rpn.charAt(i)) == 0) {
+                //создаем еще подцикл, который собирает в операнд число из ОПН
                 while (rpn.charAt(i) != ' ' && getPriority(rpn.charAt(i)) == 0) {
-//                    operand.append(rpn.charAt(i++));
-                    operand += rpn.charAt(i++);
-                    if(i == rpn.length()) {
+                    operand.append(rpn.charAt(i++));
+                    if (i == rpn.length()) {
                         break;
                     }
                 }
-//                resultStack.push(Double.parseDouble(operand.toString()));
-                resultStack.push(Double.parseDouble(operand));
-//                operand = new StringBuilder();
-                operand = "";
+                //засовываем операнд в стэк
+                resultStack.push(Double.parseDouble(operand.toString()));
+                //чистим переменную для нового значения
+                operand = new StringBuilder();
             }
+            //самая мякотка. если символ похож на операцию, то...
             if (getPriority(rpn.charAt(i)) > 1) {
+                //выдергиваем из стэка операнды
                 double b = resultStack.pop();
                 double a = resultStack.pop();
-
+                //производим операцию согласно символу и результат запихиваем обратно в стэк
                 switch (rpn.charAt(i)) {
                     case '+':
                         resultStack.push(a + b);
@@ -109,35 +121,34 @@ public class Cawculator {
                         resultStack.push(res);
                         break;
                 }
-
             }
-
-
         }
-
         return resultStack.pop();
     }
-
-    private static String extendingExpression (String expression) {
-//        StringBuilder result = new StringBuilder();
-        String result = "";
+    //Костыль для того что вернуть новое выражение в виде строки. Суть в добавлении доп нолей, которые в последстввии
+    //помогут распознавать унарные минуса в выражении
+    private static String extendingExpressionForUnaryMinus(String expression) {
+        //создаем стрингбилдер
+        StringBuilder result = new StringBuilder();
+        //в цикле пробегаем по всему выражению по каждому символу
         for (int i = 0; i < expression.length(); i++) {
+            //в случае нахождения минуса начинаем дополнительные проверки
             if (expression.charAt(i) == '-') {
+                //если присутствуем минус в начале выражения, то смело добавляем ноль
                 if (i == 0) {
-//                    result.append('0');
-                    result += '0';
-                } else if ( expression.charAt(i - 1) == '(') {
-//                    result.append('0');
-                    result += '0';
+                    result.append('0');
+                    //если минус стоит после скобки, то так же, отринув сомнения, добавляем ноль
+                } else if (expression.charAt(i - 1) == '(') {
+                    result.append('0');
                 }
             }
-//            result.append(expression.charAt(i));
-            result += expression.charAt(i);
+            //собираем строку в порядке очереди
+            result.append(expression.charAt(i));
         }
-//        return result.toString();
-        return result;
+        //возвращаем получившегося мутанта
+        return result.toString();
     }
-
+    //Возвращает значение приоритета присущее символам из выражения
     private static int getPriority(Character character) {
         switch (character) {
             case ')':
@@ -156,5 +167,4 @@ public class Cawculator {
                 return 0;
         }
     }
-
 }
--- src/main/Main.java
+++ src/main/Main.java
@@ -12,22 +12,15 @@ public class Main {
         System.out.println("Welcome to CAWculator!");
         System.out.println("For exit write \"CAW\"");
         System.out.print("Write yours expression: ");
-
         //первичный ввод из консоли
         String expression = reader.readLine();
         //вечный цикл с условием для остановки по желанию
         while (!expression.equalsIgnoreCase("caw")) {
-            //перевод введеного выражения в обратную польскую нотацию(ОПН) (что бы понять магию нужно провалится в метод expressionToRPN())
-            String rpnExpression = Cawculator.expressionToRPN(expression);
-            //чисто для самопроверки. выводим строку в виде ОПН
-            System.out.println("RPN expression is: " + rpnExpression);
-            //вывод ответа на основе ОПН. так же что бы понять магию проваливаемся в метод
-            double d = Cawculator.rpnToAnswer(rpnExpression);
-            System.out.println("Answer is: " + expression + "=" + d);
-//            System.out.println("Answer is: " + expression + "=" + Cawculator.rpnToAnswer(rpnExpression));
+            //вывод ответа на основе ОПН(Обратная Польская Нотация). Что бы понять магию проваливаемся в метод
+            System.out.println("Answer is: " + expression + "=" + Cawculator.cawculateIt(expression));
             //предложение ввести еще выражение
             System.out.print("Write yours expression: ");
-            //ввод нового выпажения
+            //ввод нового выражения
             expression = reader.readLine();
         }
         System.out.println("CaW-cAw!");
